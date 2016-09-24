51b47682bd3d4df32131916139634ecbdc2bf828
Dobrin Dachkinov <Dobrin Dachkinov>
2016-09-24 16:27:05 +0300

    working with printing of choice only once

--- PrintNgo.py
+++ PrintNgo.py
@@ -2,20 +2,13 @@ import os
 
 file_list = os.listdir("C:/Python27/Programs/PrintNgo/Files2print")
 print ("List of available documents to print" '\n')
+
 enum_list = ('\n'.join('{}: {}'.format(*k) for k in enumerate(file_list)))
 print(enum_list)
 
-while True:
-   try:
-      user_choice = int(input('\n' "Documents # you want to print: "))
-      break
-   except ValueError:
-            print("Sorry, that isn't a valid choice")
-            continue
-   else:
-     break
-   if user_choice >= 0:
-     print (file_list[user_choice])
-      #os.startfile("C:/Python27/Programs/PrintNgo/Files2print/doc0.docx", "print")
-   else:
-      print ("This # does not reffer to a document")
+user_choice = input('\n' "Documents # you want to print: ")
+copies = input("How many copies would you like from each: ")
+
+current_choice = file_list[user_choice]
+current_file = os.startfile("C:/Python27/Programs/PrintNgo/Files2print/"+current_choice, "print")
+
